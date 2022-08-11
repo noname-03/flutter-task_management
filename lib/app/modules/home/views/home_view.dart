a7e4b1b404fbae971491a25e4a70402b38c61119
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2022-08-11 15:45:59 +0700

    add firbase base

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:task_management/app/routes/app_pages.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -14,9 +15,46 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            context.isPhone
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 40,
+                        ),
+                      ),
+                      Text(
+                        'Please Sign In',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Start Journey With Us',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  )
+                : const SizedBox(),
+            Image.asset(
+              'assets/images/login.png',
+              height: Get.height * 0.5,
+            ),
+            FloatingActionButton.extended(
+              onPressed: () => Get.toNamed(Routes.LOGIN),
+              label: const Text('Sign In With Google'),
+            )
+          ],
         ),
       ),
     );
